fd2a65c99a40e71d918c2037a10b1d88c2ec09d6
Aykut5647 <157456537+Aykut5647@users.noreply.github.com>
2024-01-25 10:35:36 +0200

    Add files via upload

---
+++ 25.01.2024.;/25.01.2024.;/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _25._01._2024._
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kolko danni shte vivejdash");
+            int n=int.Parse(Console.ReadLine());
+            string[] name =new string[n];
+            string[] predmet = new string[n];
+            int[] ocenki = new int[n];
+
+
+            for(int i=0; i<n; i++) 
+            {
+            Console.Write("Vivedi ime");
+            name[i] =Console.ReadLine();
+            Console.Write("Kakiv predmet imash");
+            predmet[i]=Console.ReadLine();
+            Console.Write("Kolko broq ocenki imash");
+            ocenki[i] =int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
